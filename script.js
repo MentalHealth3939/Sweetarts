a61de285a6e0f4118fef848fffc2e7a5cd639a94
MentalHealth3939 <personalprojects3939@gmail.com>
2021-03-09 18:14:32 -0600

    Add existing file

---
+++ script.js
@@ -0,0 +1,45 @@
+const topTrending = document.querySelector(".top-trending")
+const topTrendingBottom = document.querySelector(".top-trending-bottom")
+const topElements = document.querySelectorAll(".trending-item")
+const featureBlocks = document.querySelectorAll(".feature-block")
+
+topTrending.addEventListener("mouseover", () => {
+    topElements.forEach(item => {
+        item.classList.toggle('hover')
+    })
+    topTrendingBottom.classList.toggle('hover')
+})
+
+topTrending.addEventListener("mouseout", () => {
+    topElements.forEach(item => {
+        item.classList.toggle('hover')
+    })
+    topTrendingBottom.classList.toggle('hover')
+})
+
+featureBlocks.forEach(item => {
+
+    item.addEventListener("mouseover", () => {
+        item.classList.toggle('hover')
+
+        let textChildren = item.children[0].children
+        console.log(textChildren)
+
+        for (let item of textChildren) {
+            item.classList.toggle('hover')
+        }
+    })
+
+    item.addEventListener("mouseout", () => {
+        item.classList.toggle('hover')
+
+        let textChildren = item.children[0].children
+        console.log(textChildren)
+
+        for (let item of textChildren) {
+            item.classList.toggle('hover')
+        }
+    })
+
+})
+
